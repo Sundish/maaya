c5c4694f1d598bc90042339ee3c03e2522edd836
Sundish <caliduz@gmail.com>
2021-03-08 18:10:07 -0600

    Fix missing '[]'

--- config.h
+++ config.h
@@ -5,4 +5,4 @@ static unsigned int y_double_padding = 20;
 static const char background_color[] = "white";
 static const char foreground_color[] = "black";
 
-static const char output_image = "test.jpg";
+static const char output_image[] = "test.jpg";
