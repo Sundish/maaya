593c114f94933f4febc4b47ba9b5c1bb278c58fa
Sundish <caliduz@gmail.com>
2021-03-08 23:16:39 -0600

    File Initialization

---
+++ calc.c
@@ -0,0 +1,41 @@
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+
+#include "calc.h"
+
+const int BAKTUN = 144000;
+const int KATUN = 7200;
+const int TUN = 360;
+const int UINAL = 20;
+const int KIN = 1;
+
+double
+get_working_time (void) {
+     struct tm new_epoch;
+     new_epoch.tm_year = 2012 - 1900;
+     new_epoch.tm_mon = 11;
+     new_epoch.tm_mday = 22;
+     new_epoch.tm_hour = 0;
+     new_epoch.tm_min = 0;
+     new_epoch.tm_sec = 0;
+     new_epoch.tm_isdst = 0;
+     return ceil(0.000011574*difftime(time(NULL),
+                                      mktime(&new_epoch)));
+}
+
+struct mayaT
+from_working_time_to_maya (double wt) {
+     struct mayaT mt;
+     double a, b, c, d;
+     a = fmod(wt,144000);
+     b = fmod(a, 7200);
+     c = fmod(b, 360);
+     d = fmod(c, 20);
+     mt.mayaT_baktun = (wt/BAKTUN);
+     mt.mayaT_katun = (a/KATUN);
+     mt.mayaT_tun = (b/TUN);
+     mt.mayaT_uinal = (c/UINAL);
+     mt.mayaT_kin = d;
+     return mt;
+}
---
+++ calc.h
@@ -0,0 +1,11 @@
+
+struct mayaT {
+  double mayaT_baktun;
+  double mayaT_katun;
+  double mayaT_tun;
+  double mayaT_uinal;
+  double mayaT_kin;
+};
+
+double get_working_time (void);
+struct mayaT from_working_time_to_maya (double wt);
